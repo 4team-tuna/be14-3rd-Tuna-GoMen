77f2592ba7ecf47ad63453eed0b8152499afbea1
JK-LEE98 <abc9541@gmail.com>
2025-04-14 16:22:28 +0900

    별점 리뷰 작성페이지 UI 구현 완료

--- frontend/gomen/src/router/index.js
+++ frontend/gomen/src/router/index.js
@@ -34,6 +34,9 @@ import ReportForm from '@/views/Report/ReportForm.vue'
 // 지원자 페이지
 import ApplicantListPage from "@/views/Applicant/ApplicantListPage.vue";
 
+// 별점 리뷰 페이지
+import ReviewWritePage from '@/views/Review/ReviewWritePage.vue'
+
 const routes = [
   { path: '/mail', component: MessageInbox},
   // 기본 진입 시 관리자 홈으로 리다이렉트
@@ -106,6 +109,13 @@ const routes = [
     path: '/mentorlist/status',
     name: 'MentorStatus',
     component: () => import('@/views/MentorList/MentorStatusPage.vue') 
+  },
+
+  // 별점 리뷰 페이지
+  {
+    path: '/review/write',
+    name:'ReviewWritePage',
+    component: () => import('@/views/Review/ReviewWritePage.vue')
   }
 ]
 
