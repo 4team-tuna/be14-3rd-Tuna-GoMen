bc2cf7e9e72fa0defec7667943e4d6914b67a4fe
SungMin-hub <sungmin806@gmail.com>
2025-04-14 20:36:19 +0900

    멘토링 공간 폐쇄

--- frontend/gomen/src/views/Mentoring/MentoringPage.vue
+++ frontend/gomen/src/views/Mentoring/MentoringPage.vue
@@ -11,6 +11,7 @@
 
 <script setup>
 import { ref, onMounted } from 'vue'
+import { useRouter } from 'vue-router'
 import MentorView from '@/views/Mentoring/MentorView.vue'
 import MenteeView from '@/views/Mentoring/MenteeView.vue'
 import api from '@/api'
@@ -18,17 +19,25 @@ import api from '@/api'
 const user = JSON.parse(localStorage.getItem('user'))
 const isMentor = ref(false)
 const spaceId = ref(null)
+const router = useRouter()
 
 onMounted(async () => {
   try {
     const res = await api.get(`/mentoringSpaces?userId=${user.id}`)
     const mySpace = res.data[0]
     if (!mySpace) return
+    console.log('mySpace:', mySpace)
+    console.log('🔍 is_activated:', mySpace.is_activated)
+    if (mySpace.is_activated !== 'Y') {
+      alert('⛔ 종료된 멘토링 공간입니다.')
+      return router.push('/main')
+    }
 
     spaceId.value = mySpace.id  // ✅ 확정적으로 id 사용
     isMentor.value = String(mySpace.mentor_id) === String(user.id) // ✅ 타입 강제 일치
   } catch (e) {
     console.error('멘토링 공간 조회 실패:', e)
+    router.push('/main')
   }
 })
 </script>
